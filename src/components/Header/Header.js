98e240557c67e6ea8574f3935b7ca68cf94612a5
ansarali41 <ansar.ict14@gmail.com>
2020-09-16 11:07:43 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Form, Nav, Navbar, Button, FormControl } from 'react-bootstrap';
+import logo from '../../Images/Logo.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <Navbar bg="white" variant="light">
+                <Navbar.Brand href="#home"><img src={logo} alt="" /></Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search Your Destination" className="mr-sm-2" />
+                    </Form>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="#new">News</Nav.Link>
+                    <Nav.Link href="#destination">Destination</Nav.Link>
+                    <Nav.Link href="#blog">Blog</Nav.Link>
+                    <Nav.Link href="#features">Contact</Nav.Link>
+                    <Nav.Link href="#login"><Button variant="warning">Login</Button></Nav.Link>
+                </Nav>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
